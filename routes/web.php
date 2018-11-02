269d903307a9e8d1ee7b005ebff7de66a0526886
zzb <zhangzhebin@uqian.me>
2018-11-02 14:01:20 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+
     });
 });
