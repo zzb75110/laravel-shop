4e52860ed7e404f2c0f65227ec4e496cbd3a072a
zzb <zhangzhebin@uqian.me>
2018-11-08 12:37:44 +0000

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\UserAddress;
+use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        UserAddress::class => UserAddressPolicy::class,
     ];
 
     /**
