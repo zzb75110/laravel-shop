a77da96a48102bdabf8f8312f78bf9d6823f4b2c
zzb <zhangzhebin@uqian.me>
2018-11-13 09:54:41 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+    Route::get('products', 'ProductsController@index')->name('products.index');
+    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
     Route::group(['middleware' => 'email_verified'], function() {
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
