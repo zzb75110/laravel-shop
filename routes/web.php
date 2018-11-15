021cd31f20d33000a179e12d05cb74f8e8663099
zzb <zhangzhebin@uqian.me>
2018-11-15 02:42:08 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('products', 'ProductsController@index')->name('products.index');
-    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
     Route::group(['middleware' => 'email_verified'], function() {
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
@@ -31,7 +30,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
+    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 });
 
 Route::get('test',function (){
