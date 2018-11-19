24cc4a95668af799dbd299e23bd05f5c0cef2274
zzb <zhangzhebin@uqian.me>
2018-11-19 10:15:27 +0000

    管理后台 - 拒绝退款

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -83,7 +83,65 @@
                     <td>{{ $order->ship_data['express_no'] }}</td>
                 </tr>
             @endif
+            @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+                <tr>
+                    <td>退款状态：</td>
+                    <td colspan="2">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}，理由：{{ $order->extra['refund_reason'] }}</td>
+                    <td>
+                        <!-- 如果订单退款状态是已申请，则展示处理按钮 -->
+                        @if($order->refund_status === \App\Models\Order::REFUND_STATUS_APPLIED)
+                            <button class="btn btn-sm btn-success" id="btn-refund-agree">同意</button>
+                            <button class="btn btn-sm btn-danger" id="btn-refund-disagree">不同意</button>
+                        @endif
+                    </td>
+                </tr>
+            @endif
             </tbody>
         </table>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        // 不同意 按钮的点击事件
+        $('#btn-refund-disagree').click(function() {
+            // 注意：Laravel-Admin 的 swal 是 v1 版本，参数和 v2 版本的不太一样
+            swal({
+                title: '输入拒绝退款理由',
+                input: 'text',
+                showCancelButton: true,
+                confirmButtonText: "确认",
+                cancelButtonText: "取消",
+            }).then(function (inputValue) {
+                if (inputValue.dismiss == "cancel") {
+                    return;
+                }
+                if (!inputValue.value) {
+                    swal('理由不能为空', '', 'error')
+                    return;
+                }
+                // Laravel-Admin 没有 axios，使用 jQuery 的 ajax 方法来请求
+                $.ajax({
+                    url: '{{ route('admin.orders.handle_refund', [$order->id]) }}',
+                    type: 'POST',
+                    data: JSON.stringify({   // 将请求变成 JSON 字符串
+                        agree: false,  // 拒绝申请
+                        reason: inputValue.value,
+                        // 带上 CSRF Token
+                        // Laravel-Admin 页面里可以通过 LA.token 获得 CSRF Token
+                        _token: LA.token,
+                    }),
+                    contentType: 'application/json',  // 请求的数据格式为 JSON
+                    success: function (data) {  // 返回成功时会调用这个函数
+                        swal({
+                            title: '操作成功',
+                            type: 'success'
+                        }, function() {
+                            // 用户点击 swal 上的 按钮时刷新页面
+                            location.reload();
+                        });
+                    }
+                });
+            })
+        });
+    });
+</script>
