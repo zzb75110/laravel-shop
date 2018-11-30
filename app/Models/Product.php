e8f24782c8fde5e9409ca155f1dfd6018b1ee247
vagrant <vagrant@homestead>
2018-11-30 08:10:24 +0000

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,11 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
     public static $typeMap = [
-        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_NORMAL       => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL      => '秒杀商品',
     ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
@@ -21,7 +23,7 @@ class Product extends Model
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
-    // 与商品SKU关联
+
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
@@ -49,6 +51,11 @@ class Product extends Model
     public function properties()
     {
         return $this->hasMany(ProductProperty::class);
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 
     public function getGroupedPropertiesAttribute()
